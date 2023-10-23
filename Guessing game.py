10367a3d684d841df7c3f4291e57db21824e4137
Streikin <109292425+Streikin@users.noreply.github.com>
2023-10-24 01:47:09 +0200

    Update Guessing game.py

--- Guessing game.py
+++ Guessing game.py
@@ -1,20 +1,49 @@
-from builtins import int
 import random
-
-won = False
 guess_count = 1
+global guess
 
-random_number_magnitude = int(input("Enter max value: "))
-number = random.randint(1, random_number_magnitude)
-guess = int(input("Number generated. Make your first guess: "))
-while not won:
-  if number > guess:
-      guess = int(input("Too low. Try again: "))
-      guess_count += 1
+def make_guess():
+   global guess
+   while True:
+    try:
+        guess = int(input("Make a guess: "))
+    except ValueError:
+        print("\033[91mInvalid input type.\033[0m") 
+    else:
+        break
+
+while True:
+    try:
+        number_magnitude = int(input("Enter max value (int): "))
+        if number_magnitude == 0:
+           print("\033[91mNumber can't be smaller than 1.\033[0m")
+           exit()
+    except ValueError:
+        print("\033[91mInvalid input type.\033[0m")
+    else:
+        break
+
+number = random.randint(1, number_magnitude)
+print(f"Number generated (1-{number_magnitude}).")
+make_guess()
+while True:
+  if guess > number_magnitude:
+    print(f"\033[91mThe number can't be bigger than {number_magnitude}.\033[0m")
+    make_guess()
+    guess_count += 1
+  elif guess < 1:
+    print("\033[91mThe number can't be smaller than 1.\033[0m")
+    make_guess()
+    guess_count += 1  
+  elif number > guess:
+    print("Too low.")
+    make_guess()
+    guess_count += 1
   elif number < guess:
-     guess = int(input("Too high. Try again: "))
-     guess_count += 1
+    print("Too high.")
+    make_guess()
+    guess_count += 1
   else:
-      points = random_number_magnitude / guess_count
-      print(f"\033[92mYou won! {number} was the number. You won with {guess_count} guesses out of {random_number_magnitude} numbers. That's a score of {points:.0f} points.\033[0m")
-      won = True
+    points = number_magnitude / guess_count
+    print(f"\033[92mYou won with {guess_count} guesses out of {number_magnitude} numbers.\nThat's a score of {points:.0f} points.\033[0m")
+    break
